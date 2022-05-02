5d567415a8b776e2dc0da3b3150057742506b38c
Anuj Jain <jainanuj744@gmail.com>
2022-05-02 22:53:45 +0530

    Used props in Functional Component
    Used props in Functional Component

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -2,10 +2,41 @@ import ExpenseItem from "./components/ExpenseItem";
 import "./index.css";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 600,
+      date: new Date(2021, 5, 30),
+    },
+    {
+      title: "New TV",
+      amount: 700,
+      date: new Date(2021, 6, 25),
+    },
+    {
+      title: "New Bike",
+      amount: 800,
+      date: new Date(2021, 7, 16),
+    },
+  ];
   return (
     <div className="index">
       <h1>Hello World</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem() {
-    return <h2>Expense Item works!!</h2>
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+//   const expenseDate = new Date(2022, 6, 16);
+//   const expenseTitle = "Car Insurance Waale";
+//   const expenseAmount = 542;
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item_price">${props.amount}</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
