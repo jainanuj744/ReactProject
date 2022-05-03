70579378b4f70772e30fbf571c67aaa28696203d
Anuj Jain <jainanuj744@gmail.com>
2022-05-04 00:29:06 +0530

    Date Type Change
    Date Type Change

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-//   const expenseDate = new Date(2022, 6, 16);
-//   const expenseTitle = "Car Insurance Waale";
-//   const expenseAmount = 542;
+  const expenseMonth = props.date.toLocaleString("en-US", { month: "long" });
+  const expenseDay = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const expenseYear = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseDay}</div>
+        <div>{expenseYear}</div>
+      </div>
       <div className="expense-item_description">
         <h2>{props.title}</h2>
       </div>
