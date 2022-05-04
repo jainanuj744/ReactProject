3abbadbcdae06467ae2b71e69e427d3b4e638942
Anuj Jain <jainanuj744@gmail.com>
2022-05-04 11:00:03 +0530

    Another Component of Date
    Another Component of Date

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseMonth = props.date.toLocaleString("en-US", { month: "long" });
-  const expenseDay = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const expenseYear = props.date.getFullYear();
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{expenseMonth}</div>
-        <div>{expenseDay}</div>
-        <div>{expenseYear}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item_description">
         <h2>{props.title}</h2>
       </div>
