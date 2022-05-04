a0c989478f7353798a1a3ceeb2d14a264269db99
Anuj Jain <jainanuj744@gmail.com>
2022-05-04 23:36:21 +0530

    Section-3 completed
    Section-3 completed

--- react-guide/src/components/Expenses/ExpenseItem.js
+++ react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item_description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
-      <div className="expense-item_price">${props.amount}</div>
-    </div>
+      <div className="expense-item__price">${props.amount}</div>
+    </Card>
   );
 }
 
--- react-guide/src/components/Expenses/Expenses.js
+++ react-guide/src/components/Expenses/Expenses.js
@@ -1,30 +1,26 @@
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div>
-      <div className="expenses">
+    <Card className="expenses">
         <ExpenseItem
           title={props.item[0].title}
           amount={props.item[0].amount}
           date={props.item[0].date}
         ></ExpenseItem>
-      </div>
-      <div className="expenses">
         <ExpenseItem
           title={props.item[1].title}
           amount={props.item[1].amount}
           date={props.item[1].date}
         ></ExpenseItem>
-      </div>
-      <div className="expenses">
         <ExpenseItem
           title={props.item[2].title}
           amount={props.item[2].amount}
           date={props.item[2].date}
         ></ExpenseItem>
-      </div>
-    </div>
+    </Card>
   );
 }
 
